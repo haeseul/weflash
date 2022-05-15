b2ac4706173b6407ad95439ce1cabc79e7ff1306
haeseul <ksw06205@naver.com>
2022-05-15 15:05:30 +0900

    캐러셀 구현중

--- assets/script/aaa.js
+++ assets/script/aaa.js
@@ -1,22 +1,29 @@
 ;(function () {
     'use strict'
-
+  
     const get = (target) => {
-        return document.querySelector(target)
+      return document.querySelector(target)
     }
-
+  
+    const cellCount = 6
+    let selectIndex = 0 // carousel의 첫번째부터 보여주기
+    const prevButton = get('.prev_button')
+    const nextButton = get('.next_button')
     const carousel = get('.carousel')
-
-    const init = () => {
-        for (let index = 1; index <= 16; index++) {
-            const carouselItem = document.createElement('div');
-            const carouselImage = document.createElement('img');
-            const carouselText = document.createTextNode('안녕하세요');
-            carouselItem.appendChild(carouselImage);
-            carouselImage.src = ""
-            carousel.appendChild(carouselItem);
-        }
+  
+    const rotateCarousel = () => {
+      const angle = (selectIndex / cellCount) * -360  // item들과 반대 값으로 rotate 해줘야 함
+      carousel.style.transform = `translateZ(-346px) rotateY(${angle}deg)`
     }
-
-    init()
-})()
+  
+    prevButton.addEventListener('click', () => {
+      selectIndex--
+      rotateCarousel()
+    })
+  
+    nextButton.addEventListener('click', () => {
+      selectIndex++
+      rotateCarousel()
+    })
+  })()
+  
--- assets/script/test_full_script.js
+++ assets/script/test_full_script.js
@@ -4,21 +4,61 @@
     const get = (target) => {
         return document.querySelector(target)
     }
+    
+    const carousel = get('.carousel');
+    const carouselItem = carousel.querySelectorAll('.carousel.item');
+    const itemWidth = carousel.offsetWidth;
 
-    const carousel = get('.carousel')
-    const prevButton = get('.carousel_prev_button')
-    const nextButton = get('.carousel_nest_button')
+    const itemCount = 16;
+    const theta = 360 / itemCount;
+    let selectedIndex = 0;
+    const radius = Math.round(itemWidth / 2 / Math.tan(Math.PI / itemCount));
+    console.log(radius);
+    const prevButton = get('.carousel_prev_button');
+    const nextButton = get('.carousel_nest_button');
+
+    const rotateCarousel = () => {
+        let angle = theta * selectedIndex * -1;
+        carousel.style.transform = "translateZ(" + -radius + "px) rotateY(" + angle + "deg)";
+    }
+
+    // prevButton.addEventListener("click", () => {
+    //     selectedIndex--;
+    //     rotateCarousel();
+    // })
+    
+    // nextButton.addEventListener("click", () => {
+    //     selectedIndex++;
+    //     rotateCarousel();
+    // })
+
+    const changeCarousel = () => {
+        for (let i = 0; i < carouselItem.length; i++) {
+            let item = carouselItem[i];
+            if (i < itemCount) {
+                item.style.opacity = 1;
+                let itemAngle = theta * i;
+                item.style.transform = "rotateY(" + itemAngle + "deg) translateZ(" + radius + "px)";
+            } else {
+                item.style.opacity = 0;
+                item.style.transform = "none";
+            }
+        }
+        rotateCarousel();
+        // console.log(carouselItem.length);
+    }
 
     const init = () => {
-        for (let index = 1; index <= 16; index++) {
-            const carouselItem = document.createElement('div');
-            const carouselImage = document.createElement('img');
-            carouselImage.src = `https://via.placeholder.com/200x150?text=img${index}`;
-            carouselItem.className = 'carousel_item';
-            carouselItem.appendChild(carouselImage);
-            carousel.appendChild(carouselItem);
-
-
+        for (let index = 1; index <= itemCount; index++) {
+            // carouselItem = document.createElement('div');
+            // carouselImage = document.createElement('img');
+            // carouselImage.src = `https://via.placeholder.com/200x150?text=img${index}`;
+            // carouselItem.className = 'carousel_item';
+            // carouselItem.appendChild(carouselImage);
+            // carousel.appendChild(carouselItem);
+            // carouselItem = get('.carousel_item');
+            
+            changeCarousel();
         }
         // const six = get('.carousel_item:nth-child(16)');
         // six.style.transform = 'rotateY(22.5deg)';
