3d6a988daea9134c1ce21e34171984e39c8f8813
haeseul <ksw06205@naver.com>
2022-05-15 16:39:29 +0900

    전체 결과 페이지 완성

--- assets/script/aaa.js
+++ assets/script/aaa.js
@@ -1,35 +1,29 @@
 ;(function () {
+    'use strict'
+
     const carousel = document.querySelector(".carousel");
-    const cells = carousel.querySelectorAll(".carousel__cell");
-    const cellCount = cells.length; // cellCount set from cells-range input value
+    let cells, cellCount;
     let selectedIndex = 0;
     const cellWidth = carousel.offsetWidth;
-    // const cellHeight = carousel.offsetHeight;
-    // var isHorizontal = true;
-    // var rotateFn = isHorizontal ? "rotateY" : "rotateX";
-    let radius, theta;theta = 360 / cellCount
-    // console.log( cellWidth, cellHeight );
+    let radius, theta;
     
     function rotateCarousel() {
       let angle = theta * selectedIndex * -1;
       carousel.style.transform = `translateZ(${-radius}px) rotateY(${angle}deg)`;
     }
     
-    let prevButton = document.querySelector(".previous-button");
+    let prevButton = document.querySelector(".carousel_prev_button");
     prevButton.addEventListener("click", function () {
       selectedIndex--;
       rotateCarousel();
     });
     
-    let nextButton = document.querySelector(".next-button");
+    let nextButton = document.querySelector(".carousel_next_button");
     nextButton.addEventListener("click", function () {
       selectedIndex++;
       rotateCarousel();
     });
     
-    // var cellsRange = document.querySelector(".cells-range");
-    // cellsRange.addEventListener("change", changeCarousel);
-    // cellsRange.addEventListener("input", changeCarousel);
     
     function changeCarousel() {
       for (let i = 0; i < cells.length; i++) {
@@ -51,25 +45,21 @@
       rotateCarousel();
     }
     
-    // var orientationRadios = document.querySelectorAll('input[name="orientation"]');
-    // (function () {
-    //   for (var i = 0; i < orientationRadios.length; i++) {
-    //     var radio = orientationRadios[i];
-    //     radio.addEventListener("change", onOrientationChange);
-    //   }
-    // })();
+    const init = () => {
+        for (let index = 1; index <= 16; index++) {
+            const carouselItem = document.createElement('div');
+            const carouselImage = document.createElement('img');
+            carouselImage.src = `https://via.placeholder.com/190x120?text=img${index}`;
+            carouselItem.className = 'carousel_cell';
+            carouselItem.appendChild(carouselImage);
+            carousel.appendChild(carouselItem);
+
+            cells = carousel.querySelectorAll(".carousel_cell");
+            cellCount = cells.length;
+        }
+        changeCarousel();
+    }
     
-    // function onOrientationChange() {
-    //   var checkedRadio = document.querySelector(
-    //     'input[name="orientation"]:checked'
-    //   );
-    //   isHorizontal = checkedRadio.value == "horizontal";
-    //   rotateFn = isHorizontal ? "rotateY" : "rotateX";
-    //   changeCarousel();
-    // }
-    
-    // set initials
-    changeCarousel();
-    
+    init();
   })()
   
--- assets/script/test_full_script.js
+++ assets/script/test_full_script.js
@@ -1,70 +1,65 @@
 ;(function () {
     'use strict'
 
-    const get = (target) => {
-        return document.querySelector(target)
-    }
-    
-    const carousel = get('.carousel');
-    const carouselItem = carousel.querySelectorAll('.carousel.item');
-    const itemWidth = carousel.offsetWidth;
-
-    const itemCount = 16;
-    const theta = 360 / itemCount;
+    const carousel = document.querySelector(".carousel");
+    let cells, cellCount;
     let selectedIndex = 0;
-    const radius = Math.round(itemWidth / 2 / Math.tan(Math.PI / itemCount));
-    console.log(radius);
-    const prevButton = get('.carousel_prev_button');
-    const nextButton = get('.carousel_nest_button');
-
-    const rotateCarousel = () => {
-        let angle = theta * selectedIndex * -1;
-        carousel.style.transform = "translateZ(" + -radius + "px) rotateY(" + angle + "deg)";
-    }
-
-    // prevButton.addEventListener("click", () => {
-    //     selectedIndex--;
-    //     rotateCarousel();
-    // })
+    const cellWidth = carousel.offsetWidth;
+    let radius, theta;
     
-    // nextButton.addEventListener("click", () => {
-    //     selectedIndex++;
-    //     rotateCarousel();
-    // })
-
-    const changeCarousel = () => {
-        for (let i = 0; i < carouselItem.length; i++) {
-            let item = carouselItem[i];
-            if (i < itemCount) {
-                item.style.opacity = 1;
-                let itemAngle = theta * i;
-                item.style.transform = "rotateY(" + itemAngle + "deg) translateZ(" + radius + "px)";
-            } else {
-                item.style.opacity = 0;
-                item.style.transform = "none";
-            }
-        }
-        rotateCarousel();
-        // console.log(carouselItem.length);
+    function rotateCarousel() {
+      let angle = theta * selectedIndex * -1;
+      carousel.style.transform = `translateZ(${-radius}px) rotateY(${angle}deg)`;
     }
-
+    
+    let prevButton = document.querySelector(".carousel_prev_button");
+    prevButton.addEventListener("click", function () {
+      selectedIndex--;
+      rotateCarousel();
+    });
+    
+    let nextButton = document.querySelector(".carousel_next_button");
+    nextButton.addEventListener("click", function () {
+      selectedIndex++;
+      rotateCarousel();
+    });
+    
+    
+    function changeCarousel() {
+      for (let i = 0; i < cells.length; i++) {
+        radius = Math.round(cellWidth / 2 / Math.tan(Math.PI / cellCount));
+        theta = 360 / cellCount;
+        let cell = cells[i];
+        if (i < cellCount) {
+          // visible cell
+          cell.style.opacity = 1;
+          let cellAngle = theta * i;
+          cell.style.transform = `rotateY(${cellAngle}deg) translateZ(${radius}px)`;
+        } else {
+          // hidden cell
+          cell.style.opacity = 0;
+          cell.style.transform = "none";
+        }
+      }
+      console.log(cells.length)
+      rotateCarousel();
+    }
+    
     const init = () => {
-        for (let index = 1; index <= itemCount; index++) {
-            // carouselItem = document.createElement('div');
-            // carouselImage = document.createElement('img');
-            // carouselImage.src = `https://via.placeholder.com/200x150?text=img${index}`;
-            // carouselItem.className = 'carousel_item';
-            // carouselItem.appendChild(carouselImage);
-            // carousel.appendChild(carouselItem);
-            // carouselItem = get('.carousel_item');
-            
-            changeCarousel();
+        for (let index = 1; index <= 16; index++) {
+            const carouselItem = document.createElement('div');
+            const carouselImage = document.createElement('img');
+            carouselImage.src = `https://via.placeholder.com/190x120?text=img${index}`;
+            carouselItem.className = 'carousel_cell';
+            carouselItem.appendChild(carouselImage);
+            carousel.appendChild(carouselItem);
+
+            cells = carousel.querySelectorAll(".carousel_cell");
+            cellCount = cells.length;
         }
-        // const six = get('.carousel_item:nth-child(16)');
-        // six.style.transform = 'rotateY(22.5deg)';
-        // six.style.backgroundColor='white';
+        changeCarousel();
     }
-
-
-    init()
-})()
+    
+    init();
+  })()
+  
