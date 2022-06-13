cc6f90c52fffb3b5331104f2015f704ffc98f7d9
haeseul <ksw06205@naver.com>
2022-06-13 22:35:45 +0900

    6.13 수정

--- games/flags/script.js
+++ games/flags/script.js
@@ -4,8 +4,13 @@
     const get = (target) => document.querySelector(target)
 
     let trial = 5
+    var time = 4
 
+    const $startButton = get('.start_button')
+    const $startModal = get('.start')
     const $statement = get('.statement')
+    const $progressbar = get('.progressbar')
+    const $bar = get('.bar')
     const $characters = get('.characters')
     const $char1 = get('.char1')
     const $char2 = get('.char2')
@@ -18,6 +23,28 @@
     const controls_up = new Array("올리고", "올리지 말고")
     const controls_down = new Array("내리고", "내리지 말고")
     const action = new Array("앉아", "일어나", "앉지마", "일어나지 마")
+
+    const start = () => {
+        $startButton.addEventListener('click', () => {
+            $startModal.classList.add("hide");
+        })
+    }
+
+    const clickLeft = () => {
+        if ($left_button.classList["value"] == "left_button click") {
+            $left_button.classList.toggle('click')
+        } else if ($left_button.classList["value"] == "left_button") {
+            $left_button.classList.toggle('click')
+        }
+    }
+    const clickRight = () => {
+        if ($right_button.classList["value"] == "right_button click") {
+            $right_button.classList.toggle('click')
+        } else if ($right_button.classList["value"] == "right_button") {
+            $right_button.classList.toggle('click')
+        }
+    }
+
 
     const randomItem = (arr) => {
         return arr[Math.floor(Math.random() * arr.length)]
@@ -46,7 +73,15 @@
     }
 
     const init = () => {
+        start()
         makeStatement()
+
+        $left_button.addEventListener('click', () => {
+            clickLeft()
+        })
+        $right_button.addEventListener('click', () => {
+            clickRight()
+        })
     }
 
     init()
