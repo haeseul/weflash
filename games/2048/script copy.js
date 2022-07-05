a93b0d1d0149b3526a864aab117654b97674222f
haeseul <ksw06205@naver.com>
2022-07-05 17:39:16 +0900

    .

--- games/2048/script copy.js
+++ games/2048/script copy.js
@@ -5,7 +5,7 @@
     const getAll = (target) => document.querySelectorAll(target)
 
     const $table = get('.table')
-    let $score = get('score')
+    let $score = get('.score')
 
     let board = Array(Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0))
     let score
@@ -16,7 +16,7 @@
         let count = 0
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
-                if (board[i][j] == 0) count++
+                if (board[i][j] == 0) count++;
             }
         }
         while(true) {
@@ -39,11 +39,72 @@
     const update = () => {
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
-                if (board[i][j] == 0) continue
                 let cell = document.getElementById(i + '' + j)
                 cell.innerHTML = board[i][j]==0 ? '' : board[i][j]
-                cell.classList.add('color-' + cell.innerHTML)
+                coloring(cell)
             }
+        }
+        $score.innerHTML = score
+    }
+
+    const coloring = (cell) => {
+        let cellNum = parseInt(cell.innerHTML)
+        switch (cellNum) {
+            case 0:
+            case 2: 
+                cell.style.background= "#eee4da"
+                cell.style.color= "#776e65"
+                break;
+            case 4: 
+                cell.style.background= "#eee1c9"
+                cell.style.color= "#776e65"
+                break;
+            case 8: 
+                cell.style.background= "#f3b27a"
+                cell.style.color= "#fff"
+                break;
+            case 16: 
+                cell.style.background= "#f69664"
+                cell.style.color= "#fff"
+                break;
+            case 32: 
+                cell.style.background= "#f77c5f"
+                cell.style.color= "#fff"
+                break;
+            case 64: 
+                cell.style.background= "#f75f3b"
+                cell.style.color= "#fff"
+                break;
+            case 128: 
+                cell.style.background= "#edd073"
+                cell.style.color= "#776e65"
+                break;
+            case 256: 
+                cell.style.background= "#edcc62"
+                cell.style.color= "#776e65"
+                break;
+            case 512: 
+                cell.style.background= "#edc950"
+                cell.style.color= "#776e65"
+                break;
+            case 1024: 
+                cell.style.background= "#edc53f"
+                cell.style.color= "#776e65"
+                break;
+            case 2048: 
+                cell.style.background= "#edc22e"
+                cell.style.color= "#776e65"
+                break;
+        
+            default:
+                if (cellNum > 2048) {
+                    cell.style.background= "#E51A1A"
+                    cell.style.color= "#776e65"
+                } else {
+                    cell.style.background= "#9e9184"
+                    cell.style.color= "#776e65"
+                }
+                break;
         }
     }
 
@@ -53,42 +114,114 @@
         return (rand == 0)? 4 : 2
     }
 
+    const getMaxNum = () => {
+        let ret = 0
+        for (let i = 0; i < 4; i++) {
+            for (let j = 1; j < 4; i++) {
+                if (board[i][j] > ret) {
+                    ret = board[i][j]
+                }
+            }
+        }
+        return ret
+    }
+
+    const checkGameOver = () => {
+        for(let i=0;i<4;i++){
+            let colCheck = board[i][0];
+            if(colCheck==0) return;
+            for(let j=1;j<4;j++){
+                if(board[i][j]==colCheck || board[i][j]==0) return;
+                else colCheck = board[i][j];
+            }
+        }
+        for(let i=0;i<4;i++){
+            let rowCheck = board[0][i];
+            if(rowCheck==0) return;
+            for(let j=1;j<4;j++){
+                if(board[j][i]==rowCheck || board[j][i]==0) return;
+                else rowCheck = board[j][i];
+            }
+        }
+
+        alert("[Game Over]\nMax: "+ getMaxNum() + "\nScore" + score)
+        init()
+    }
+
     const rotate = (n) => {
-        
+        while(n--) {
+            let tmpBoard = Array(Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0))
+            for(let i=0;i<4;i++)
+                for(let j=0;j<4;j++)
+                    tmpBoard[i][j]=board[i][j];
+            for(let i=0;i<4;i++)
+                for(let j=0;j<4;j++)
+                    board[j][3-i]=tmpBoard[i][j];
+        }
     }
 
     const move = () => {
-        
+        let isMoved = false
+        let isPlused = Array(Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0))
+        for(let i=1;i<4;i++){
+            for(let j=0;j<4;j++){
+                if(board[i][j]==0) continue;
+                let tempY = i-1;
+                while(tempY>0 && board[tempY][j]==0) tempY--;
+                if(board[tempY][j]==0){
+                    board[tempY][j]=board[i][j];
+                    board[i][j]=0;
+                    isMoved=true;
+                }
+                else if(board[tempY][j]!=board[i][j]){
+                    if(tempY+1==i) continue;
+                    board[tempY+1][j]=board[i][j];
+                    board[i][j]=0;
+                    isMoved=true;
+                }
+                else{
+                    if(isPlused[tempY][j]==0){
+                        board[tempY][j]*=2;
+                        score+=board[tempY][j];
+                        board[i][j]=0;
+                        isPlused[tempY][j]=1;
+                        isMoved=true;
+                    }
+                    else{
+                        board[tempY+1][j]=board[i][j];
+                        board[i][j]=0;
+                        isMoved=true;
+                    }
+                }
+            }
+        }
+        if (isMoved) generate()
+        else checkGameOver()
     }
 
     
     const keyDownEventHandler = (e) => {
         switch (e.keyCode) {
             case 38: // up
-                console.log(e.keyCode)
-                move()
+                move();
                 break;
             case 40: // down
-                console.log(e.keyCode)
-                rotate(2)
-                move()
-                rotate(2)
+                rotate(2);
+                move();
+                rotate(2);
                 break;
             case 37: // left
-                console.log(e.keyCode)
-                rotate(1)
-                move()
-                rotate(3)
+                rotate(1);
+                move();
+                rotate(3);
                 break;
             case 39: // right
-                console.log(e.keyCode)
-                rotate(3)
-                move()
-                rotate(1)
-                break;
-            default:
+                rotate(3);
+                move();
+                rotate(1);
                 break;
         }
+        update()
     }
 
 
@@ -104,11 +237,11 @@
         }
         // 2개의 칸 채우기
         for (let i = 0; i < 2; i++) {
-            let rand = parseInt(Math.random() * 16)
-            let x = rand % 4
-            let y = parseInt(rand / 4)
-            if (board[y][x] == 0) board[y][x] = choose2or4()
-            else i--
+            let rand = parseInt(Math.random() * 16);
+            let x = rand % 4;
+            let y = parseInt(rand / 4);
+            if (board[y][x] == 0) board[y][x] = choose2or4();
+            else i--;
         }
         update()
     }
