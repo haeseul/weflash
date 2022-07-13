a9861b48e1af21fddc9963b53e92763516d6cf18
haeseul <ksw06205@naver.com>
2022-07-13 16:57:59 +0900

    flappybird 수정중..

--- games/flappyBird/app.js
+++ games/flappyBird/app.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const bird = document.querySelector('.bird')
-    const gameDisplay = document.querySelector('.game-container')
-    const ground = document.querySelector('.ground')
+    const get = (target) => document.querySelector(target)
+
+    const start_screen = get('.start-screen')
+    const game_container = get('.game-container')
+    const end_screen = get('.end-screen')
+
+    const bird = get('.bird')
+    const gameDisplay = get('.game-container')
+    const ground = get('.ground')
 
     let birdLeft = 220
     let birdBottom = 100
@@ -72,12 +78,26 @@ document.addEventListener('DOMContentLoaded', () => {
         let timerId = setInterval(moveObstacle, 20)
         if (!isGameOver) setTimeout(generateObstacle, 3500)  // 3초마다 장애물 생기기
     }
-    generateObstacle()
 
     function gameOver() {
         clearInterval(gameTimerId)
         console.log('game over')
         isGameOver = true
         document.removeEventListener('keyup', control)
+        game_container.classList.remove('active')
+        end_screen.classList.add('active')
     }
+
+    generateObstacle()
+
+    get('.start-btn').addEventListener('click', () => {
+        start_screen.classList.remove('active')
+        game_container.classList.add('active')
+    })
+
+    get('.replay-btn').addEventListener('click', () => {
+        generateObstacle()
+        end_screen.classList.remove('active')
+        game_container.classList.add('active')
+    })
 })
